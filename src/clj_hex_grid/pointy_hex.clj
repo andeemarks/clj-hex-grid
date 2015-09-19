f18ca2b7842804945ae39d419cc8b45bcf760eef
Andy Marks <amarks@thoughtworks.com>
2015-09-19 22:01:15 +1000

    De-duping of hex_corner impl

--- src/clj_hex_grid/pointy_hex.clj
+++ src/clj_hex_grid/pointy_hex.clj
@@ -1,4 +1,5 @@
-(ns clj-hex-grid.pointy-hex)
+(ns clj-hex-grid.pointy-hex
+	(:require [clj-hex-grid.hex :as hex]))
 
 (defn height
 	([{size :size}] (* size 2))
@@ -16,24 +17,6 @@
 	([]	(vertical-distance-to-adjacent {:size 1}))
 	([attrs] (* 0.75 (height attrs))))
 
-(defn- calc_corner_x [center size angle_rad]
-	(+ (get center :x) (* (or size 1) (Math/cos angle_rad))))
-
-(defn- calc_corner_y [center size angle_rad]
-	(+ (get center :y) (* (or size 1) (Math/sin angle_rad))))
-
-(defn- calc_hex_corner 
-	[center corner size]
-	(let [angle_deg (+ 30 (* corner 60))
-				angle_rad (/ Math/PI (* 180 angle_deg))
-				corner_x (calc_corner_x center size angle_rad)
-				corner_y (calc_corner_x center size angle_rad)]
-				{:x corner_x :y corner_y}))
-
 (defn hex_corner 
 	[{center :center corner :corner size :size}]
-	(if (or (nil? center) (nil? corner))
-		(throw Exception))
-	(if (or (< corner 0) (> corner 5))
-		(throw Exception))
-	(calc_hex_corner center corner size))
+	(hex/calc_hex_corner center corner size 30))
