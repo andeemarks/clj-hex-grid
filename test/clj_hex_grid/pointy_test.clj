85f3c28aa58ecb8f11c2a8fd1d5676ebc95c0432
Andy Marks <amarks@thoughtworks.com>
2015-09-19 16:18:54 +1000

    Split tests for pointies and flats

--- test/clj_hex_grid/pointy_test.clj
+++ test/clj_hex_grid/pointy_test.clj
@@ -1,35 +1,6 @@
-(ns clj-hex-grid.core-test
-	(:require [clj-hex-grid.flat-hex :as flat])
+(ns clj-hex-grid.pointy-test
 	(:require [clj-hex-grid.pointy-hex :as pointy])
   (:use [midje.sweet]))
-
-(facts "for flat-topped hexen"
-
-		(facts "when calculating widths"
-			(fact "default size is 1"
-				(flat/width) => 2)
-			(fact "width is twice the size"
-				(flat/width {:size 1}) => 2
-				(flat/width {:size 2}) => 4))
-
-		(facts "when calculating heights"
-			(fact "default size is 1"
-				(flat/height) => (Math/sqrt 3))
-			(fact "height is the width multiplied by half the square root of 3"
-				(flat/height {:size 1}) => (Math/sqrt 3)
-				(flat/height {:size 2}) => (* 2 (Math/sqrt 3))))
-
-
-	(facts "when calculating distances"
-		(fact "horizontal-distance between adjacent hexes is 3/4 of width"
-			(flat/horizontal-distance-to-adjacent) => (* 2 0.75)
-			(flat/horizontal-distance-to-adjacent {:size 1}) => (* 2 0.75)))
-
-		(fact "vertical-distance between adjacent hexes is same as height"
-			(flat/vertical-distance-to-adjacent {:size 2}) => (* 2 (flat/height))			
-			(flat/vertical-distance-to-adjacent) => (flat/height))
-		
-)
 
 (facts "for pointy-topped hexen"
 
