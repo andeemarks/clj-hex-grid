c5eaa05e8d96149d6349fc34c71e8c811fc91c61
Andy Marks <amarks@thoughtworks.com>
2015-09-19 15:42:42 +1000

    Height calcs done - impl in tests though

--- src/clj_hex_grid/core.clj
+++ src/clj_hex_grid/core.clj
@@ -4,6 +4,10 @@
 	([{size :size}] (* size 2))
 	([] (width {:size 1})))
 
+(defn height
+	([{size :size}] (* (/ (Math/sqrt 3) 2) (width {:size size})))
+	([] (height {:size 1})))
+
 (defn distance-to-adjacent 
 	([]	(distance-to-adjacent {:size 1}))
 	([attrs] (double (* (/ 3 4) (width attrs)))))
--- test/clj_hex_grid/core_test.clj
+++ test/clj_hex_grid/core_test.clj
@@ -2,16 +2,25 @@
 	(:require [clj-hex-grid.core :as hex])
   (:use [midje.sweet]))
 
-(facts "about hex sizing..."
+(facts "for flat-topped hexen"
+		(facts "when calculating widths"
+			(fact "default size is 1"
+				(hex/width) => 2)
+			(fact "width is twice the size"
+				(hex/width {:size 1}) => 2
+				(hex/width {:size 2}) => 4))
 
-	(fact "default width is 1"
-		(hex/width) => 2)
-	(fact "width is twice the size"
-		(hex/width {:size 1}) => 2
-		(hex/width {:size 2}) => 4))
+		(facts "when calculating heights"
+			(fact "default size is 1"
+				(hex/height) => (Math/sqrt 3))
+			(fact "height is the width multiplied by half the square root of 3"
+				(hex/height {:size 1}) => (Math/sqrt 3)
+				(hex/height {:size 2}) => (* 2 (Math/sqrt 3))))
 
-(facts "about hex spacing..."
-	(fact "distance between adjacent hexes is 3/4 of width"
-		(hex/distance-to-adjacent) => (* 2 0.75)
-		(hex/distance-to-adjacent {:size 1}) => (* 2 0.75))
-	)
+
+	(facts "about hex spacing"
+		(fact "horizontal-distance between adjacent hexes is 3/4 of width"
+			(hex/distance-to-adjacent) => (* 2 0.75)
+			(hex/distance-to-adjacent {:size 1}) => (* 2 0.75))
+		)
+)
