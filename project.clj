12c18a554d9221dceb020b0f879c6f5d4c8528d2
Andy Marks <amarks@thoughtworks.com>
2015-09-20 20:36:15 +1000

    Trying different approach to access lein midje via Snap CI

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:dev {:dependencies [[midje "1.7.0"]]}}	
-  :user {:plugins [[lein-midje "3.1.3"]]}
+  :profiles {:dev {:dependencies [[midje "1.7.0"]]
+  								 :plugins [[lein-midje "3.1.3"]]}}	
   :dependencies [[org.clojure/clojure "1.7.0"]])
