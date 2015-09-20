c3a04ca9e451f049ed577f4cc14d647c05c1e2e2
Andy Marks <amarks@thoughtworks.com>
2015-09-20 20:51:59 +1000

    Starting to build out compojure version

--- project.clj
+++ project.clj
@@ -1,8 +1,17 @@
 (defproject clj-hex-grid "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A basic Clojure library for working with hexagonal grids"
+  :url "https://github.com/andeemarks/clj-hex-grid/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:dev {:dependencies [[midje "1.7.0"]]
-  								 :plugins [[lein-midje "3.1.3"]]}}	
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :profiles {
+  	:dev {
+  		:dependencies [
+  			[midje "1.7.0"]
+				[compojure "1.4.0"]
+				]	
+			:plugins [
+				[lein-midje "3.1.3"]
+				]}}	
+	:dependencies [
+		[org.clojure/clojure "1.7.0"]
+		])
