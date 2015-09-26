c169f65f6afa40adadac7f21ed70c2e9acd67ce8
Andy Marks <amarks@thoughtworks.com>
2015-09-26 21:16:02 +1000

    Now using docstring formatting

--- src/clj_hex_grid/coordinate.clj
+++ src/clj_hex_grid/coordinate.clj
@@ -1,24 +1,30 @@
 (ns clj-hex-grid.coordinate)
 
-(defn cube_to_offset_odd_q [{x :x y :y z :z}]
-	; z + (x - (x&1)) / 2
+(defn cube_to_offset_odd_q 
+	"z + (x - (x&1)) / 2"
+	[{x :x y :y z :z}]
 	{:col x :row (+ z (/ (- x (bit-and x 1)) 2))})
 
-(defn cube_to_offset_even_q [{x :x y :y z :z}]
-	; z + (x + (x&1)) / 2
+(defn cube_to_offset_even_q 
+	"z + (x + (x&1)) / 2"
+	[{x :x y :y z :z}]
 	{:col x :row (+ z (/ (+ x (bit-and x 1)) 2))})
 
-(defn cube_to_offset_even_r [{x :x y :y z :z}]
-	; x + (z + (z&1)) / 2
+(defn cube_to_offset_even_r 
+	"x + (z + (z&1)) / 2"
+	[{x :x y :y z :z}]
 	{:row z :col (+ x (/ (+ z (bit-and z 1)) 2))})
 
-(defn cube_to_offset_odd_r [{x :x y :y z :z}]
-	; col => x + (z - (z&1)) / 2
+(defn cube_to_offset_odd_r 
+	"col => x + (z - (z&1)) / 2"
+	[{x :x y :y z :z}]
 	{:row z :col (+ x (/ (- z (bit-and z 1)) 2))})
 
-(defn offset_even_q_to_cube [{row :row col :col}]
-	; z => row - (col + (col&1)) / 2
-	; y => -x-z
+(defn offset_even_q_to_cube 
+	"z => row - (col + (col&1)) / 2
+	y => -x-z
+	x => col"
+	[{row :row col :col}]
 	(let [x col
 				z (- row (/ (+ col (bit-and col 1)) 2))
 				y (- (- 0 x) z)]
