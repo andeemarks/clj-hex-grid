6dcacba0381a45d00253404d81d73cd68cf04054
Andy Marks <amarks@thoughtworks.com>
2015-09-26 20:59:03 +1000

    Finished all cube->offset conversions

--- src/clj_hex_grid/coordinate.clj
+++ src/clj_hex_grid/coordinate.clj
@@ -1,13 +1,17 @@
 (ns clj-hex-grid.coordinate)
 
 (defn cube_to_offset_odd_q [{x :x y :y z :z}]
-	{:col x :row "1"})
+	; z + (x - (x&1)) / 2
+	{:col x :row (+ z (/ (- x (bit-and x 1)) 2))})
 
 (defn cube_to_offset_even_q [{x :x y :y z :z}]
-	{:col x :row "1"})
+	; z + (x + (x&1)) / 2
+	{:col x :row (+ z (/ (+ x (bit-and x 1)) 2))})
 
 (defn cube_to_offset_even_r [{x :x y :y z :z}]
-	{:row z})
+	; x + (z + (z&1)) / 2
+	{:row z :col (+ x (/ (+ z (bit-and z 1)) 2))})
 
 (defn cube_to_offset_odd_r [{x :x y :y z :z}]
-	{:row z})
+	; x + (z - (z&1)) / 2
+	{:row z :col (+ x (/ (- z (bit-and z 1)) 2))})
--- test/clj_hex_grid/coordinate_test.clj
+++ test/clj_hex_grid/coordinate_test.clj
@@ -4,18 +4,27 @@
 
 (facts "when converting from cube coordinates"
 	(facts "to offset even-q"
+		(fact "the offset row is assigned z + (x + (x&1)) / 2"
+			(coord/cube_to_offset_even_q {:x 1 :y 2 :z 3}) => (contains {:row 4}))
 		(fact "the offset column is assigned the cube x"
 			(coord/cube_to_offset_even_q {:x 1 :y 2 :z 3}) => (contains {:col 1})))
 
 	(facts "to offset odd-q"
+		(fact "the offset row is assigned z + (x - (x&1)) / 2"
+			(coord/cube_to_offset_odd_q {:x 1 :y 2 :z 3}) => (contains {:row 3}))
 		(fact "the offset column is assigned the cube x"
 			(coord/cube_to_offset_odd_q {:x 1 :y 2 :z 3}) => (contains {:col 1})))
 
 	(facts "to offset even-r"
+		(fact "the offset column is assigned x + (z + (z&1)) / 2"
+			(coord/cube_to_offset_even_r {:x 1 :y 2 :z 3}) => (contains {:col 3}))
 		(fact "the offset row is assigned the cube z"
 			(coord/cube_to_offset_even_r {:x 1 :y 2 :z 3}) => (contains {:row 3})))
 
 	(facts "to offset odd-r"
+		(fact "the offset column is assigned x + (z - (z&1)) / 2"
+			(coord/cube_to_offset_odd_r {:x 1 :y 2 :z 3}) => (contains {:col 2}))
+
 		(fact "the offset row is assigned the cube z"
 			(coord/cube_to_offset_odd_r {:x 1 :y 2 :z 3}) => (contains {:row 3})))
 
