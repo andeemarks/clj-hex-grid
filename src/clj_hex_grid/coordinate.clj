89fc30b0c5d36786914793a89bf10a0084c4d699
Andy Marks <amarks@thoughtworks.com>
2015-09-26 21:10:46 +1000

    First set of conversions from offset to cube

--- src/clj_hex_grid/coordinate.clj
+++ src/clj_hex_grid/coordinate.clj
@@ -13,5 +13,13 @@
 	{:row z :col (+ x (/ (+ z (bit-and z 1)) 2))})
 
 (defn cube_to_offset_odd_r [{x :x y :y z :z}]
-	; x + (z - (z&1)) / 2
+	; col => x + (z - (z&1)) / 2
 	{:row z :col (+ x (/ (- z (bit-and z 1)) 2))})
+
+(defn offset_even_q_to_cube [{row :row col :col}]
+	; z => row - (col + (col&1)) / 2
+	; y => -x-z
+	(let [x col
+				z (- row (/ (+ col (bit-and col 1)) 2))
+				y (- (- 0 x) z)]
+		{:x x :y y :z z}))
--- test/clj_hex_grid/coordinate_test.clj
+++ test/clj_hex_grid/coordinate_test.clj
@@ -2,12 +2,42 @@
 	(:require [clj-hex-grid.coordinate :as coord])
   (:use [midje.sweet]))
 
-(facts "when converting from cube coordinates"
+(facts "when converting from cube to offset coordinates"
 	(facts "to offset even-q"
-		(fact "the offset row is assigned z + (x + (x&1)) / 2"
+		(fact "the row is assigned z + (x + (x&1)) / 2"
 			(coord/cube_to_offset_even_q {:x 1 :y 2 :z 3}) => (contains {:row 4}))
-		(fact "the offset column is assigned the cube x"
+		(fact "the column is assigned the cube x"
 			(coord/cube_to_offset_even_q {:x 1 :y 2 :z 3}) => (contains {:col 1})))
+
+	(facts "to odd-q"
+		(fact "the row is assigned z + (x - (x&1)) / 2"
+			(coord/cube_to_offset_odd_q {:x 1 :y 2 :z 3}) => (contains {:row 3}))
+		(fact "the column is assigned the cube x"
+			(coord/cube_to_offset_odd_q {:x 1 :y 2 :z 3}) => (contains {:col 1})))
+
+	(facts "to even-r"
+		(fact "the column is assigned x + (z + (z&1)) / 2"
+			(coord/cube_to_offset_even_r {:x 1 :y 2 :z 3}) => (contains {:col 3}))
+		(fact "the row is assigned the cube z"
+			(coord/cube_to_offset_even_r {:x 1 :y 2 :z 3}) => (contains {:row 3})))
+
+	(facts "to odd-r"
+		(fact "the column is assigned x + (z - (z&1)) / 2"
+			(coord/cube_to_offset_odd_r {:x 1 :y 2 :z 3}) => (contains {:col 2}))
+
+		(fact "the row is assigned the cube z"
+			(coord/cube_to_offset_odd_r {:x 1 :y 2 :z 3}) => (contains {:row 3})))
+
+	)
+
+(facts "when converting from offset to cube coordinates"
+	(facts "from even-q"
+		(fact "the x is assigned the offset column"
+			(coord/offset_even_q_to_cube {:row 1 :col 2}) => (contains {:x 2}))
+		(fact "the z is assigned row - (col + (col&1)) / 2"
+			(coord/offset_even_q_to_cube {:row 1 :col 2}) => (contains {:z 0}))
+		(fact "the y is assigned -x-z"
+			(coord/offset_even_q_to_cube {:row 1 :col 2}) => (contains {:y -2})))
 
 	(facts "to offset odd-q"
 		(fact "the offset row is assigned z + (x - (x&1)) / 2"
@@ -27,6 +57,5 @@
 
 		(fact "the offset row is assigned the cube z"
 			(coord/cube_to_offset_odd_r {:x 1 :y 2 :z 3}) => (contains {:row 3})))
-
 
 	)
