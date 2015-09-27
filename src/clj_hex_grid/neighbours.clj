62b9ddfeae6301f0cdeb5b09a687cd2e06bd56b5
Andy Marks <amarks@thoughtworks.com>
2015-09-27 20:31:05 +1000

    Full impl of odd-r neighbours

--- src/clj_hex_grid/neighbours.clj
+++ src/clj_hex_grid/neighbours.clj
@@ -43,6 +43,23 @@
 													:southwest {:x -1 :y 0}}
 										})
 
+
+(def odd_r_neighbours {even_column 
+												{	:northeast {:x 0 :y -1} 
+													:east {:x 1 :y 0}
+													:southeast {:x 0 :y 1}
+													:southwest {:x -1 :y 1}
+													:west {:x -1 :y 0}
+													:northwest {:x -1 :y -1}}
+											odd_column 
+												{	:northeast {:x 1 :y -1} 
+													:east {:x 1 :y 0}
+													:southeast {:x 1 :y 1}
+													:southwest {:x 0 :y 1}
+													:west {:x -1 :y 0}
+													:northwest {:x 0 :y -1}}
+										})
+
 (defn neighbour_for_cube
 	""
 	[{x :x y :y z :z} orientation]
@@ -66,6 +83,15 @@
 	[{x :x y :y} orientation]
 	(let [parity (bit-and x 1)
 				offsets (get (get even_q_neighbours parity) orientation)]
+		; (println offsets)
+		{	:x (+ x (get offsets :x)) 
+			:y (+ y (get offsets :y))}))
+
+(defn neighbour_for_odd_r
+	""
+	[{x :x y :y} orientation]
+	(let [parity (bit-and y 1)
+				offsets (get (get odd_r_neighbours parity) orientation)]
 		; (println offsets)
 		{	:x (+ x (get offsets :x)) 
 			:y (+ y (get offsets :y))}))
--- test/clj_hex_grid/neighbours_test.clj
+++ test/clj_hex_grid/neighbours_test.clj
@@ -77,4 +77,34 @@
 				(neighbours/neighbour_for_even_q {:x 1 :y 0} :southwest) => {:x 0 :y 0})
 			)
 		)
+	(facts "for odd-r offset coordinates"
+		(facts "when on an even row"
+			(fact "the north eastern neighbour is y - 1"
+				(neighbours/neighbour_for_odd_r {:x 0 :y 0} :northeast) => {:x 0 :y -1})
+			(fact "the eastern neighbour is x + 1"
+				(neighbours/neighbour_for_odd_r {:x 0 :y 0} :east) => {:x 1 :y 0})
+			(fact "the south eastern neighbour is y + 1"
+				(neighbours/neighbour_for_odd_r {:x 0 :y 0} :southeast) => {:x 0 :y 1})
+			(fact "the south western neighbour is x - 1, y + 1"
+				(neighbours/neighbour_for_odd_r {:x 0 :y 0} :southwest) => {:x -1 :y 1})
+			(fact "the western neighbour is x - 1"
+				(neighbours/neighbour_for_odd_r {:x 0 :y 0} :west) => {:x -1 :y 0})
+			(fact "the north western neighbour is x - 1, y - 1"
+				(neighbours/neighbour_for_odd_r {:x 0 :y 0} :northwest) => {:x -1 :y -1})
+			)
+		(facts "when on an odd row"
+			(fact "the north eastern neighbour is x + 1"
+				(neighbours/neighbour_for_odd_r {:x 0 :y 1} :northeast) => {:x 1 :y 0})
+			(fact "the eastern neighbour is x + 1, y + 1"
+				(neighbours/neighbour_for_odd_r {:x 0 :y 1} :east) => {:x 1 :y 1})
+			(fact "the south eastern neighbour is x + 1, y + 1"
+				(neighbours/neighbour_for_odd_r {:x 0 :y 1} :southeast) => {:x 1 :y 2})
+			(fact "the south western neighbour is y + 1"
+				(neighbours/neighbour_for_odd_r {:x 0 :y 1} :southwest) => {:x 0 :y 2})
+			(fact "the western neighbour is x - 1, y + 1"
+				(neighbours/neighbour_for_odd_r {:x 0 :y 1} :west) => {:x -1 :y 1})
+			(fact "the north western neighbour is y - 1"
+				(neighbours/neighbour_for_odd_r {:x 0 :y 1} :northwest) => {:x 0 :y 0})
+			)
+		)
 	)
