fc88e4ed4bc362e14e5eba9632090c06932c850c
Andy Marks <amarks@thoughtworks.com>
2015-09-27 19:52:06 +1000

    Partial impl of neighbours for odd_q

--- src/clj_hex_grid/neighbours.clj
+++ src/clj_hex_grid/neighbours.clj
@@ -1,18 +1,34 @@
 (ns clj-hex-grid.neighbours)
 
-(def neighbours {	:north {:x 0 :y 1 :z -1} 
-									:south {:x 0 :y -1 :z 1}
-									:northeast {:x 1 :y 0 :z -1}
-									:northwest {:x -1 :y 1 :z 0}
-									:southeast {:x 1 :y -1 :z 0}
-									:southwest {:x -1 :y 0 :z 1}
-									})
+(def cube_neighbours {:north {:x 0 :y 1 :z -1} 
+											:south {:x 0 :y -1 :z 1}
+											:northeast {:x 1 :y 0 :z -1}
+											:northwest {:x -1 :y 1 :z 0}
+											:southeast {:x 1 :y -1 :z 0}
+											:southwest {:x -1 :y 0 :z 1}
+										})
 
-(defn neighbour_for
+(def odd_q_neighbours {:north {:x 0 :y -1} 
+											:south {:x 0 :y 1}
+											:northeast {:x 1 :y -1}
+											:northwest {:x -1 :y -1}
+											:southeast {:x 1 :y 0}
+											:southwest {:x -1 :y 0}
+										})
+
+(defn neighbour_for_cube
 	""
 	[{x :x y :y z :z} orientation]
-	(let [offsets (get neighbours orientation)]
+	(let [offsets (get cube_neighbours orientation)]
 		; (println offsets)
 		{	:x (+ x (get offsets :x)) 
 			:y (+ y (get offsets :y)) 
 			:z (+ z (get offsets :z))}))
+
+(defn neighbour_for_odd_q
+	""
+	[{x :x y :y} orientation]
+	(let [offsets (get odd_q_neighbours orientation)]
+		; (println offsets)
+		{	:x (+ x (get offsets :x)) 
+			:y (+ y (get offsets :y))}))
--- test/clj_hex_grid/neighbours_test.clj
+++ test/clj_hex_grid/neighbours_test.clj
@@ -3,18 +3,34 @@
   (:use [midje.sweet]))
 
 (facts "when determining neighbours"
-	(facts "for flat hexen"
+	(facts "for cube coordinates"
 		(fact "the northern neighbour is y + 1, z - 1"
-			(neighbours/neighbour_for {:x 0 :y 0 :z 0} :north) => {:x 0 :y 1 :z -1})
+			(neighbours/neighbour_for_cube {:x 0 :y 0 :z 0} :north) => {:x 0 :y 1 :z -1})
 		(fact "the southern neighbour is y - 1, z + 1"
-			(neighbours/neighbour_for {:x 0 :y 0 :z 0} :south) => {:x 0 :y -1 :z 1})
+			(neighbours/neighbour_for_cube {:x 0 :y 0 :z 0} :south) => {:x 0 :y -1 :z 1})
 		(fact "the north eastern neighbour is x + 1, z - 1"
-			(neighbours/neighbour_for {:x 0 :y 0 :z 0} :northeast) => {:x 1 :y 0 :z -1})
+			(neighbours/neighbour_for_cube {:x 0 :y 0 :z 0} :northeast) => {:x 1 :y 0 :z -1})
 		(fact "the north western neighbour is y + 1, x - 1"
-			(neighbours/neighbour_for {:x 0 :y 0 :z 0} :northwest) => {:x -1 :y 1 :z 0})
+			(neighbours/neighbour_for_cube {:x 0 :y 0 :z 0} :northwest) => {:x -1 :y 1 :z 0})
 		(fact "the south eastern neighbour is x + 1, z - 1"
-			(neighbours/neighbour_for {:x 0 :y 0 :z 0} :southeast) => {:x 1 :y -1 :z 0})
+			(neighbours/neighbour_for_cube {:x 0 :y 0 :z 0} :southeast) => {:x 1 :y -1 :z 0})
 		(fact "the south western neighbour is z + 1, x - 1"
-			(neighbours/neighbour_for {:x 0 :y 0 :z 0} :southwest) => {:x -1 :y 0 :z 1})
+			(neighbours/neighbour_for_cube {:x 0 :y 0 :z 0} :southwest) => {:x -1 :y 0 :z 1})
+		)
+	(facts "for odd-q offset coordinates"
+		(facts "when on an even column"
+			(fact "the northern neighbour is y - 1"
+				(neighbours/neighbour_for_odd_q {:x 0 :y 0} :north) => {:x 0 :y -1})
+			(fact "the southern neighbour is y + 1"
+				(neighbours/neighbour_for_odd_q {:x 0 :y 0} :south) => {:x 0 :y 1})
+			(fact "the north eastern neighbour is x + 1, y - 1"
+				(neighbours/neighbour_for_odd_q {:x 0 :y 0} :northeast) => {:x 1 :y -1})
+			(fact "the north western neighbour is x - 1, y - 1"
+				(neighbours/neighbour_for_odd_q {:x 0 :y 0} :northwest) => {:x -1 :y -1})
+			(fact "the south eastern neighbour is x + 1"
+				(neighbours/neighbour_for_odd_q {:x 0 :y 0} :southeast) => {:x 1 :y 0})
+			(fact "the south western neighbour is x - 1"
+				(neighbours/neighbour_for_odd_q {:x 0 :y 0} :southwest) => {:x -1 :y 0})
+			)
 		)
 	)
