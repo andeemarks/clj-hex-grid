285539c8c306f273920a1321cc63a0bbe6fe9254
Andy Marks <amarks@thoughtworks.com>
2015-09-27 19:36:19 +1000

    Finished neighbours for cube coordinates

--- src/clj_hex_grid/neighbours.clj
+++ src/clj_hex_grid/neighbours.clj
@@ -1,6 +1,12 @@
 (ns clj-hex-grid.neighbours)
 
-(def neighbours {:north {:x 0 :y 1 :z -1} :south {:x 0 :y -1 :z 1}})
+(def neighbours {	:north {:x 0 :y 1 :z -1} 
+									:south {:x 0 :y -1 :z 1}
+									:northeast {:x 1 :y 0 :z -1}
+									:northwest {:x -1 :y 1 :z 0}
+									:southeast {:x 1 :y -1 :z 0}
+									:southwest {:x -1 :y 0 :z 1}
+									})
 
 (defn neighbour_for
 	""
--- test/clj_hex_grid/neighbours_test.clj
+++ test/clj_hex_grid/neighbours_test.clj
@@ -8,5 +8,13 @@
 			(neighbours/neighbour_for {:x 0 :y 0 :z 0} :north) => {:x 0 :y 1 :z -1})
 		(fact "the southern neighbour is y - 1, z + 1"
 			(neighbours/neighbour_for {:x 0 :y 0 :z 0} :south) => {:x 0 :y -1 :z 1})
+		(fact "the north eastern neighbour is x + 1, z - 1"
+			(neighbours/neighbour_for {:x 0 :y 0 :z 0} :northeast) => {:x 1 :y 0 :z -1})
+		(fact "the north western neighbour is y + 1, x - 1"
+			(neighbours/neighbour_for {:x 0 :y 0 :z 0} :northwest) => {:x -1 :y 1 :z 0})
+		(fact "the south eastern neighbour is x + 1, z - 1"
+			(neighbours/neighbour_for {:x 0 :y 0 :z 0} :southeast) => {:x 1 :y -1 :z 0})
+		(fact "the south western neighbour is z + 1, x - 1"
+			(neighbours/neighbour_for {:x 0 :y 0 :z 0} :southwest) => {:x -1 :y 0 :z 1})
 		)
 	)
