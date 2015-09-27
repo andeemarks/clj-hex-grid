cde10c6acfbc6d1b04b014c8298badc301317ef4
Andy Marks <amarks@thoughtworks.com>
2015-09-27 20:37:24 +1000

    Full impl of even-r neighbours

--- src/clj_hex_grid/neighbours.clj
+++ src/clj_hex_grid/neighbours.clj
@@ -43,7 +43,6 @@
 													:southwest {:x -1 :y 0}}
 										})
 
-
 (def odd_r_neighbours {even_column 
 												{	:northeast {:x 0 :y -1} 
 													:east {:x 1 :y 0}
@@ -58,6 +57,22 @@
 													:southwest {:x 0 :y 1}
 													:west {:x -1 :y 0}
 													:northwest {:x 0 :y -1}}
+										})
+
+(def even_r_neighbours {even_column 
+												{	:northeast {:x 1 :y -1} 
+													:east {:x 1 :y 0}
+													:southeast {:x 1 :y 1}
+													:southwest {:x 0 :y -1}
+													:west {:x -1 :y 0}
+													:northwest {:x 0 :y -1}}
+											odd_column 
+												{	:northeast {:x 0 :y -1} 
+													:east {:x 1 :y 0}
+													:southeast {:x 0 :y 1}
+													:southwest {:x -1 :y 1}
+													:west {:x -1 :y 0}
+													:northwest {:x -1 :y -1}}
 										})
 
 (defn neighbour_for_cube
@@ -92,6 +107,15 @@
 	[{x :x y :y} orientation]
 	(let [parity (bit-and y 1)
 				offsets (get (get odd_r_neighbours parity) orientation)]
+		; (println offsets)
+		{	:x (+ x (get offsets :x)) 
+			:y (+ y (get offsets :y))}))
+
+(defn neighbour_for_even_r
+	""
+	[{x :x y :y} orientation]
+	(let [parity (bit-and y 1)
+				offsets (get (get even_r_neighbours parity) orientation)]
 		; (println offsets)
 		{	:x (+ x (get offsets :x)) 
 			:y (+ y (get offsets :y))}))
