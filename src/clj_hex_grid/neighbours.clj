e6337bd272518f857f79708e7911809429fec6c3
Andy Marks <amarks@thoughtworks.com>
2015-09-27 19:16:15 +1000

    Fake impl of neighbour_for

--- src/clj_hex_grid/neighbours.clj
+++ src/clj_hex_grid/neighbours.clj
@@ -1,1 +1,6 @@
 (ns clj-hex-grid.neighbours)
+
+(defn neighbour_for
+	""
+	[{x :x y :y z :z} orientation]
+	{:x 0 :y 1 :z -1})
--- test/clj_hex_grid/neighbours_test.clj
+++ test/clj_hex_grid/neighbours_test.clj
@@ -1,3 +1,10 @@
 (ns clj-hex-grid.neighbours-test
 	(:require [clj-hex-grid.neighbours :as neighbours])
   (:use [midje.sweet]))
+
+(facts "when determining neighbours"
+	(facts "for flat hexen"
+		(fact "the northern neighbour is y + 1, z -1"
+			(neighbours/neighbour_for {:x 0 :y 0 :z 0} :north) => {:x 0 :y 1 :z -1})
+		)
+	)
