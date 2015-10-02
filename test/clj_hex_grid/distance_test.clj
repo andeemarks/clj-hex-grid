6f8b1f76262d3172defd07a928b3c03ffdaa663f
Andy Marks <amarks@thoughtworks.com>
2015-10-02 20:58:59 +1000

    Now testing distances for all cube neighbours

--- test/clj_hex_grid/distance_test.clj
+++ test/clj_hex_grid/distance_test.clj
@@ -1,5 +1,6 @@
 (ns clj-hex-grid.distance-test
 	(:require [clj-hex-grid.distance :as distance])
+	(:require [clj-hex-grid.neighbours :as n])
   (:use [midje.sweet]))
 
 (def origin {:x 0 :y 0 :z 0})
@@ -7,4 +8,11 @@
 (facts "when determining distance"
 	(facts "for cube coordinates"
 		(fact "all neighbours are distance 1"
-			(distance/between origin {:x 1 :y 0 :z -1}) => 1)))
+			(let [neighbours (n/neighbours_for_cube origin)]
+			(distance/between origin (nth neighbours 0)) => 1
+			(distance/between origin (nth neighbours 1)) => 1
+			(distance/between origin (nth neighbours 2)) => 1
+			(distance/between origin (nth neighbours 3)) => 1
+			(distance/between origin (nth neighbours 4)) => 1
+			(distance/between origin (nth neighbours 5)) => 1))
+		))
