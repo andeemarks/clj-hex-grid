e889b6d298de49e43766732192ec89aebe76f5fd
Andy Marks <amarks@thoughtworks.com>
2015-10-28 16:01:13 +1100

    I think I have the rounding and pathing sorted for cubes now - needs more tests to be sure

--- test/clj_hex_grid/path_test.clj
+++ test/clj_hex_grid/path_test.clj
@@ -1,10 +1,19 @@
 (ns clj-hex-grid.path-test
 	(:require [clj-hex-grid.path :as path])
+	(:require [clj-hex-grid.neighbours :as n])
   (:use [midje.sweet]))
 
 (def cube_origin {:x 0 :y 0 :z 0})
 
 (facts "when calculating paths"
+	(fact "two elements in path fom a point to a neighbour"
+		(let [neighbours (n/neighbours_for_cube cube_origin)
+			  path (path/path_between cube_origin (first neighbours))]
+			(count path) => 2
+			(first path) => cube_origin
+			(second path) => (first neighbours)))
 	(fact "only one element in path from a point to itself"
-		(path/path_between cube_origin cube_origin) => (just cube_origin))
+		(let [path (path/path_between cube_origin cube_origin)]
+			(count path) => 1
+			path => (contains cube_origin)))
 	)
