6cdd3e8db7ce295034bce008dc96b8247391c86f
Andy Marks <amarks@thoughtworks.com>
2015-10-28 16:31:56 +1100

    Simplifying checkers

--- test/clj_hex_grid/path_test.clj
+++ test/clj_hex_grid/path_test.clj
@@ -10,21 +10,25 @@
 		(let [neighbours (n/neighbours_for_cube cube_origin)
 			  destination (first neighbours)
 			  path (path/path_between cube_origin destination)]
-			(count path) => 2
-			(first path) => cube_origin
-			(second path) => destination))
+			path => [cube_origin destination]))
 	(fact "only expected elements in longer horizontal path"
 		(let [destination {:x -5 :y 5 :z 0}
 			  path (path/path_between cube_origin destination)]
-			(count path) => 6
-			(nth path 0) => cube_origin
-			(nth path 1) => {:x -1 :y 1 :z 0}
-			(nth path 2) => {:x -2 :y 2 :z 0}
-			(nth path 3) => {:x -3 :y 3 :z 0}
-			(nth path 4) => {:x -4 :y 4 :z 0}
-			(nth path 5) => destination))
+			path => [cube_origin
+					{:x -1 :y 1 :z 0}
+					{:x -2 :y 2 :z 0}
+					{:x -3 :y 3 :z 0}
+					{:x -4 :y 4 :z 0}
+					destination]))
+	(fact "only expected elements in longer meandering path"
+		(let [destination {:x 4 :y -2 :z -2}
+			  path (path/path_between cube_origin destination)]
+			path => [cube_origin
+					{:x 1 :y 0 :z -1}
+					{:x 2 :y -1 :z -1}
+					{:x 3 :y -1 :z -2}
+					destination]))
 	(fact "only one element in path from a point to itself"
 		(let [path (path/path_between cube_origin cube_origin)]
-			(count path) => 1
-			path => (contains cube_origin)))
+			path => [cube_origin]))
 	)
